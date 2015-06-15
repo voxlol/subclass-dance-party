f2a7d5cbb2b10e2e33a2027125353dda43f5a4c1
MKS-students <mks.students@hackreactor.com>
2015-06-15 15:29:03 -0500

    passes same tests as before; now using old step in BlinkyDancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -8,7 +8,7 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function(){
   // call the old version of step at the beginning of any call to this new version of step
-  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  Dancer.prototype.step.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
