e4f1837557c269eb8d240def1b7664c9ad758ec8
MKS-students <mks.students@hackreactor.com>
2015-06-15 17:34:12 -0500

    Added FreshDancer w/ random rotation

---
+++ src/freshDancer.js
@@ -0,0 +1,29 @@
+var FreshDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left);
+  this.$node = $('<img class="dancer" src="images/8bitcarlton.gif">');
+  this.setPosition(top, left);
+  this.timeBetweenSteps = 250;
+
+  var direction = Math.random();
+  if(direction >= 0.5)
+    this.rotationAmount = 0.0001;
+  else
+    this.rotationAmount = -0.0001;
+}
+
+FreshDancer.prototype = Object.create(Dancer.prototype);
+FreshDancer.prototype.constructor = FreshDancer;
+
+FreshDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+
+  if(this.rotationAmount > 0)
+    this.rotationAmount += 5;
+  else
+    this.rotationAmount -= 5
+
+  var styleSettings = {
+    transform : 'rotate(' + this.rotationAmount + 'deg)'
+  };
+  this.$node.css(styleSettings);
+}
